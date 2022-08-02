f8b3b67028bacf9021c402685bd2feb21a2a2ec0
jerry <brownjer3@gmail.com>
2022-08-02 17:13:48 -0500

    complete modal unique url feature

--- src/Components/NftDetailsCard.js
+++ src/Components/NftDetailsCard.js
@@ -8,11 +8,13 @@ import Image from 'react-bootstrap/Image';
 import { transformUri, makeNftGmeLink } from './DataFormats';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { Loading } from './Components';
+
 
 
 export default function NftDetailsCard(props) {
 
-      const makeTraitRows = (traits) => {
+    const makeTraitRows = (traits) => {
         return Object.keys(traits).map((category, index) => {
             return (
                 <tr key={index.toString()}>
@@ -22,90 +24,98 @@ export default function NftDetailsCard(props) {
                 </tr>
             )
         })
-      }
+    }
 
-      const renderModal = () => {
-        if (props.nft) {
-            if (props.nft === "NFT Not Found!") {
-                return (
-                    <Modal
-                        {...props}
-                        size="lg"
-                        aria-labelledby="contained-modal-title-vcenter"
-                        centered
-                        className='text-center'
+    const renderModal = () => {
+        if (props.loading) {
+            return (
+                <Modal
+                    {...props}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                    >
+                    <Modal.Body>
+                        <Loading />
+                    </Modal.Body>
+                </Modal>
+            )
+        } else if (!props.isNftFound) {
+            return (
+                <Modal
+                    {...props}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                    className='text-center'
+                    >
+                    <Modal.Header className='bg-dark text-white text-center'>
+                        <Modal.Title className='w-100'>
+                            {props.nft}
+                        </Modal.Title>
+                        <button onClick={props.onHide} type="button" className="btn btn-close btn-close-white" aria-label="Close"></button>
+                    </Modal.Header>
+                    <Modal.Body className='bg-dark text-white'>
+                        <Image width="30%" src='https://m.media-amazon.com/images/I/81nho65ESrL._AC_SY879_.jpg' alt="NFT Not Found" />
+                    </Modal.Body>
+                </Modal>
+            )
+        } else {
+            return (
+                <Modal
+                    {...props}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                    >
+                    <Modal.Header className='bg-dark text-white'>
+                        <Modal.Title>
+                            {props.nft.name}
+                        </Modal.Title>
+                        <button onClick={props.onHide} type="button" className="btn btn-close btn-close-white" aria-label="Close"></button>
+                    </Modal.Header>
+                    <Modal.Body className='bg-dark text-white'>
+                        <Row>
+                            <Col>
+                                <Card>
+                                    <Card.Img src={transformUri(props.nft.metadataJson.image)} />
+                                </Card>
+                            </Col>
+                            <Col>
+                                <p>Rarity Rank {props.nft.rarityRank}</p>
+                                <Table hover variant="dark" size="sm">
+                                    <thead>
+                                        <tr>
+                                            <th>Type</th>
+                                            <th>Value</th>
+                                            <th>Rarity Score</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {makeTraitRows(props.nft.metadataJson.properties)}
+                                        <tr className='fw-bold text-danger'>
+                                            <td colSpan={2}>Total Rarity Score</td>
+                                            <td className='text-center'>352</td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                            </Col>
+                        </Row>
+                    </Modal.Body>
+                    <Modal.Footer className='bg-dark text-white'>
+                        <a 
+                        className='text-decoration-none' 
+                        href={makeNftGmeLink(props.nft.contractAddress, props.nft.tokenId)}
+                        target="_blank"
                         >
-                        <Modal.Header className='bg-dark text-white text-center'>
-                            <Modal.Title className='w-100'>
-                                {props.nft}
-                            </Modal.Title>
-                            <button onClick={props.onHide} type="button" className="btn btn-close btn-close-white" aria-label="Close"></button>
-                        </Modal.Header>
-                        <Modal.Body className='bg-dark text-white'>
-                            <Image width="30%" src='https://m.media-amazon.com/images/I/81nho65ESrL._AC_SY879_.jpg' alt="NFT Not Found" />
-                        </Modal.Body>
-                    </Modal>
-                )
-            } else {
-                return (
-                    <Modal
-                        {...props}
-                        size="lg"
-                        aria-labelledby="contained-modal-title-vcenter"
-                        centered
-                        >
-                        <Modal.Header className='bg-dark text-white'>
-                            <Modal.Title>
-                                {props.nft.name}
-                            </Modal.Title>
-                            <button onClick={props.onHide} type="button" className="btn btn-close btn-close-white" aria-label="Close"></button>
-                        </Modal.Header>
-                        <Modal.Body className='bg-dark text-white'>
-                            <Row>
-                                <Col>
-                                    <Card>
-                                        <Card.Img src={transformUri(props.nft.metadataJson.image)} />
-                                    </Card>
-                                </Col>
-                                <Col>
-                                    <p>Rarity Rank {props.nft.rarityRank}</p>
-                                    <Table hover variant="dark" size="sm">
-                                        <thead>
-                                            <tr>
-                                                <th>Type</th>
-                                                <th>Value</th>
-                                                <th>Rarity Score</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {makeTraitRows(props.nft.metadataJson.properties)}
-                                            <tr className='fw-bold text-danger'>
-                                                <td colSpan={2}>Total Rarity Score</td>
-                                                <td className='text-center'>352</td>
-                                            </tr>
-                                        </tbody>
-                                    </Table>
-                                </Col>
-                            </Row>
-                        </Modal.Body>
-                        <Modal.Footer className='bg-dark text-white'>
-            
-                            <a 
-                            className='text-decoration-none' 
-                            href={makeNftGmeLink(props.nft.contractAddress, props.nft.tokenId)}
-                            target="_blank"
-                            >
-                                View on Gamestop
-                                <FontAwesomeIcon icon={faUpRightFromSquare} className='mx-2' />
-                            </a>
-                        </Modal.Footer>
-                    </Modal>
-                )
-            }
-            
-        } else 
-            return <></>
-      }
+                            View on Gamestop
+                            <FontAwesomeIcon icon={faUpRightFromSquare} className='mx-2' />
+                        </a>
+                    </Modal.Footer>
+                </Modal>
+            )
+        }
+    }
 
     return(
        renderModal()
--- src/Containers/NftModalContainer.js
+++ src/Containers/NftModalContainer.js
@@ -1,43 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import NftDetailsCard from '../Components/NftDetailsCard';
-import TestModal from '../Components/TestModal';
+// import TestModal from '../Components/TestModal';
 
 const NftModalContainer = () => {
+    const { nftId } = useParams();
+    const navigate = useNavigate();
+    const handleClose = () => navigate(-1);
+
     const [modalShow, setModalShow] = useState(true);
-    // const { nftId } = useParams();
+    const [isNftFound, setIsNftFound] = useState(false);
+    const [nft, setNft] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    // const [modalShow, setModalShow] = useState(false);
-    // const [isNftFound, setIsNftFound] = useState(false);
-    // const [nft, setNft] = useState(null);
-    // const [loading, setLoading] = useState(true);
-    // const navigate = useNavigate();
+    useEffect(() => {
+       fetchNft();
+    }, [])
 
-    // const handleClose = () => navigate('/collections');
+    const fetchNft = async () => {
+        setLoading(true);
 
-    // useEffect(() => {
-    //    fetchNft();
-    // }, [])
+        let collectionId = "36fab6f7-1e51-49d9-a0be-39343abafd0f"
+        let url = `http://localhost:3001/collections/${collectionId}/nfts?serialNum=${nftId}`
+        const res = await fetch(url);
+        const data = await res.json();
 
-    // const fetchNft = async () => {
-    //     setLoading(true);
+        if (data.length == 0) {
+            setIsNftFound(false);
+            setModalShow(true);
+        } else {
+            setIsNftFound(true);
+            setNft(data[0]);
+            setModalShow(true);
+        }
 
-    //     let collectionId = "36fab6f7-1e51-49d9-a0be-39343abafd0f"
-    //     let url = `http://localhost:3001/collections/${collectionId}/nfts?serialNum=${nftId}`
-    //     const res = await fetch(url);
-    //     const data = await res.json();
-
-    //     if (data.length == 0) {
-    //         setIsNftFound(false);
-    //         setModalShow(true);
-    //     } else {
-    //         setIsNftFound(true);
-    //         setNft(data[0]);
-    //         setModalShow(true);
-    //     }
-
-    //     setLoading(false);
-    // }
+        setLoading(false);
+    }
 
     // const renderModal = () => {
     //     if (loading) {
@@ -48,11 +46,20 @@ const NftModalContainer = () => {
     // }
 
     return (
-        < TestModal         
-            show={modalShow}
-            onHide={() => setModalShow(false)}
+        // <TestModal
+        //     loading={loading}
+        //     nft={nft}
+        //     isNftFound={isNftFound}
+        //     show={modalShow}
+        //     onHide={handleClose}
+        // />
+        <NftDetailsCard 
+            nft={nft} 
+            isNftFound={isNftFound}
+            loading={loading} 
+            show={modalShow} 
+            onHide={handleClose}
         />
-        // <NftDetailsCard nft={nft} loading={loading} show={modalShow} onHide={handleClose}/>
     )
 }
 
