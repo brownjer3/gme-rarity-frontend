ad1ff759d333e7b678aff67f70cbc240a45795df
jerry <brownjer3@gmail.com>
2022-08-10 14:07:58 -0700

    simplify ttrending card styling

--- src/Components/TrendingCollectionCard.js
+++ src/Components/TrendingCollectionCard.js
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { transformUri, EthData } from "../Components/DataFormats";
 
 class TrendingCollectionCard extends Component {
@@ -13,38 +11,31 @@ class TrendingCollectionCard extends Component {
 
 		return (
 			<Col className="text-white">
-				<Card bg="dark" border="light" className="details-card h-100">
-					<Card.Img
-						className="trending-card-img-top"
-						variant="top"
-						src={transformUri(avatarUri)}
-						alt={`Trending GameStop NFT Collection - ${name}`}
-					/>
-					<Card.Body>
-						<Card.Title>{name}</Card.Title>
-						<ListGroup className="list-group-flush">
-							<ListGroup.Item className="bg-dark text-white border-0 border-bottom border-top border-secondary">
-								Collection Size: {items}
-							</ListGroup.Item>
-							<ListGroup.Item className="bg-dark text-white border-0 border-bottom border-secondary">
-								24hr Volume:
-								<EthData weiVal={volume} />
-							</ListGroup.Item>
-						</ListGroup>
-					</Card.Body>
-					<Card.Body>
-						<Link
-							className="text-white text-decoration-none"
-							to={`/collections/${slug}`}
-						>
-							Collection Details
-							<FontAwesomeIcon
-								icon={faArrowUpRightFromSquare}
-								className="mx-2"
-							/>
-						</Link>
-					</Card.Body>
-				</Card>
+				<Link
+					className="text-white text-decoration-none"
+					to={`/collections/${slug}`}
+				>
+					<Card bg="dark" border="light" className="details-card h-100">
+						<Card.Img
+							className="trending-card-img-top"
+							variant="top"
+							src={transformUri(avatarUri)}
+							alt={`Trending GameStop NFT Collection - ${name}`}
+						/>
+						<Card.Body>
+							<Card.Title>{name}</Card.Title>
+							<ListGroup className="list-group-flush">
+								<ListGroup.Item className="bg-dark text-white border-0 border-bottom border-top border-secondary">
+									Collection Size: {items}
+								</ListGroup.Item>
+								<ListGroup.Item className="bg-dark text-white">
+									24hr Volume:
+									<EthData weiVal={volume} />
+								</ListGroup.Item>
+							</ListGroup>
+						</Card.Body>
+					</Card>
+				</Link>
 			</Col>
 		);
 	}
